d1426c3e8ca756238b0b20d37522b42b6361c73f
Christopher Valerio <christopher@valerio.guru>
2020-02-02 16:27:37 -0600

    Adding init to terraform

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo -n "Building PACKER image"
+echo -n "Building PACKER image\n"
 
 docker run -it -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -v $(pwd)/packer:/packer radar/packer-builder:latest build parity-instance.json
 
-echo -n "Applying Terraform"
+echo -n "Applying Terraform\n"
 
+docker run -it -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -v $(pwd)/terraform:/terraform radar/terraform-builder:latest init
 docker run -it -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -v $(pwd)/terraform:/terraform radar/terraform-builder:latest apply -auto-approve
