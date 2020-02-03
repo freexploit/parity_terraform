4891c7d89886d6caea3dca1efff93b26c7b14443
Christopher Valerio <christopher@valerio.guru>
2020-02-02 18:21:04 -0600

    removing ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,16 +33,6 @@ resource "aws_security_group" "allow_ssh" {
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["186.15.200.209/32"]
-  }
-
-  ingress {
-    # TLS (change to whatever ports you need)
     from_port   = 30303
     to_port     = 30303
     protocol    = "tcp"
@@ -82,7 +72,7 @@ resource "aws_security_group" "parity-ports" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["186.15.200.209/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
